0128adaf3e65d348ef61960cb72d9eef1c565e47
luantran2k <tranluan2kyl@gmail.com>
2022-12-15 18:18:43 +0700

    fix build

--- src/pages/Exam/detail.tsx
+++ src/pages/Exam/detail.tsx
@@ -96,221 +96,216 @@ export default function DetailExamPage(props: IDetailExamPageProps) {
             </AppModal>
         );
     }
-    if (exam)
-        if (exam) {
-            return (
-                <Stack maxWidth="60rem" margin="0 auto" spacing={12}>
-                    <Box
-                        marginTop="2rem"
-                        padding="2rem"
-                        boxShadow="rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px"
-                        sx={{
-                            span: {
-                                fontWeight: "bold",
-                            },
-                        }}
+
+    if (exam) {
+        return (
+            <Stack maxWidth="60rem" margin="0 auto" spacing={12}>
+                <Box
+                    marginTop="2rem"
+                    padding="2rem"
+                    boxShadow="rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px"
+                    sx={{
+                        span: {
+                            fontWeight: "bold",
+                        },
+                    }}
+                >
+                    <Typography
+                        variant="h1"
+                        marginBottom="2rem"
+                        fontSize="2rem"
+                        fontWeight={400}
+                        color={grey[900]}
                     >
-                        <Typography
-                            variant="h1"
-                            marginBottom="2rem"
-                            fontSize="2rem"
-                            fontWeight={400}
-                            color={grey[900]}
-                        >
-                            {exam.title || "Không rõ"}
-                        </Typography>
-                        <Stack
-                            direction="row"
-                            spacing={2}
-                            height="4rem"
-                            marginBottom={4}
-                        >
-                            <img
-                                src={exam.owner.profileImg || BACKUP_AVATAR}
-                                style={{
-                                    height: "100%",
-                                    display: "inline-block",
-                                }}
-                            />
-                            <Stack>
-                                <UserNameButton user={exam.owner} />
-                                <Typography>
-                                    <span>Đăng ngày: </span>
-                                    {ultis.formatDate(exam.createdAt) ||
-                                        "Không rõ"}
-                                </Typography>
-                            </Stack>
+                        {exam.title || "Không rõ"}
+                    </Typography>
+                    <Stack
+                        direction="row"
+                        spacing={2}
+                        height="4rem"
+                        marginBottom={4}
+                    >
+                        <img
+                            src={exam.owner.profileImg || BACKUP_AVATAR}
+                            style={{
+                                height: "100%",
+                                display: "inline-block",
+                            }}
+                        />
+                        <Stack>
+                            <UserNameButton user={exam.owner} />
+                            <Typography>
+                                <span>Đăng ngày: </span>
+                                {ultis.formatDate(exam.createdAt) || "Không rõ"}
+                            </Typography>
                         </Stack>
+                    </Stack>
 
-                        <Typography>
-                            <span>Năm học: </span>
-                            {`${new Date(
-                                exam.date as string
-                            ).getFullYear()} - ${
-                                new Date(exam.date as string).getFullYear() + 1
-                            }` || "Không rõ"}
-                        </Typography>
-                        <Typography>
-                            <span>Cập nhật ngày: </span>
-                            {ultis.formatDate(exam.createdAt) || "Không rõ"}
-                        </Typography>
-                        <Typography>
-                            <span>Thời gian làm bài: </span>
-                            {exam.duration}
-                        </Typography>
-                        <Typography>
-                            <span>Tên kỳ thi: </span>
-                            {exam.examName}
-                        </Typography>
-                        <Typography>
-                            <span>Đơn vị phát hành: </span>
-                            {exam.publishers}
-                        </Typography>
-                        <Typography>
-                            <span>Môn: </span>
-                            {exam.subjectName}
-                        </Typography>
-                        <Typography>
-                            <span>Lớp/Trình độ: </span>
-                            {exam.grade === "university"
-                                ? "Đại học/Cao đẳng"
-                                : exam.grade}
-                        </Typography>
-                        <Typography>
-                            <span>Loại đề: </span>
-                            {exam.type === "official"
-                                ? "Chính thức"
-                                : "Không chính thức"}
-                        </Typography>
-                        <Typography>
-                            <span>Mô tả: </span>
-                            {exam.description || "Không có"}
-                        </Typography>
-                        <Stack
-                            direction="row"
-                            spacing={4}
-                            marginTop="2rem"
-                            alignItems="center"
-                        >
-                            <Star
-                                style={{
-                                    color: exam.isFavorited
-                                        ? yellow[700]
-                                        : "black",
-                                    cursor: "pointer",
-                                    fontSize: "2rem",
-                                }}
-                                onClick={async () => {
-                                    if (!userId) {
-                                        dispatch(
-                                            sendAlert({
-                                                message:
-                                                    "Bạn cần đăng nhập để thực hiện chức năng này",
-                                                time: 3,
-                                                severity: "warning",
-                                            })
-                                        );
-                                        return;
-                                    }
-                                    if (exam.isFavorited) {
-                                        const res = await request.delete(
-                                            `exams/${exam.id}/favorite?userId=${userId}`
-                                        );
-                                        if (res) {
-                                            setExam(
-                                                (preValue) =>
-                                                    ({
-                                                        ...preValue,
-                                                        isFavorited: false,
-                                                    } as IDetailExam)
-                                            );
-                                        }
-                                    } else {
-                                        const res = await request.post(
-                                            `exams/${exam.id}/favorite?userId=${userId}`,
-                                            {}
-                                        );
-                                        if (res) {
-                                            setExam(
-                                                (preValue) =>
-                                                    ({
-                                                        ...preValue,
-                                                        isFavorited: true,
-                                                    } as IDetailExam)
-                                            );
-                                        }
-                                    }
-                                }}
-                            />
-
-                            <Button
-                                variant="contained"
-                                onClick={() => {
-                                    navigate("../practice/" + exam.id);
-                                }}
-                            >
-                                Thi thử
-                            </Button>
-                            <Button
-                                variant="outlined"
-                                onClick={(e) => {
-                                    e.stopPropagation();
-                                    navigator.clipboard.writeText(
-                                        `${BASE_URL}exam/detail/${exam.id}?${
-                                            exam.isPublic
-                                                ? ""
-                                                : `securityCode=${exam.securityCode}`
-                                        }`
-                                    );
+                    <Typography>
+                        <span>Năm học: </span>
+                        {`${new Date(exam.date as string).getFullYear()} - ${
+                            new Date(exam.date as string).getFullYear() + 1
+                        }` || "Không rõ"}
+                    </Typography>
+                    <Typography>
+                        <span>Cập nhật ngày: </span>
+                        {ultis.formatDate(exam.createdAt) || "Không rõ"}
+                    </Typography>
+                    <Typography>
+                        <span>Thời gian làm bài: </span>
+                        {exam.duration}
+                    </Typography>
+                    <Typography>
+                        <span>Tên kỳ thi: </span>
+                        {exam.examName}
+                    </Typography>
+                    <Typography>
+                        <span>Đơn vị phát hành: </span>
+                        {exam.publishers}
+                    </Typography>
+                    <Typography>
+                        <span>Môn: </span>
+                        {exam.subjectName}
+                    </Typography>
+                    <Typography>
+                        <span>Lớp/Trình độ: </span>
+                        {exam.grade === "university"
+                            ? "Đại học/Cao đẳng"
+                            : exam.grade}
+                    </Typography>
+                    <Typography>
+                        <span>Loại đề: </span>
+                        {exam.type === "official"
+                            ? "Chính thức"
+                            : "Không chính thức"}
+                    </Typography>
+                    <Typography>
+                        <span>Mô tả: </span>
+                        {exam.description || "Không có"}
+                    </Typography>
+                    <Stack
+                        direction="row"
+                        spacing={4}
+                        marginTop="2rem"
+                        alignItems="center"
+                    >
+                        <Star
+                            style={{
+                                color: exam.isFavorited ? yellow[700] : "black",
+                                cursor: "pointer",
+                                fontSize: "2rem",
+                            }}
+                            onClick={async () => {
+                                if (!userId) {
                                     dispatch(
                                         sendAlert({
-                                            message: "Đã copy đường đẫn",
+                                            message:
+                                                "Bạn cần đăng nhập để thực hiện chức năng này",
                                             time: 3,
+                                            severity: "warning",
                                         })
                                     );
-                                }}
-                            >
-                                Chia sẻ
-                            </Button>
-                            {isOwner ? (
-                                <>
-                                    <Button
-                                        variant="outlined"
-                                        onClick={() => {
-                                            navigate("/user/report");
-                                        }}
-                                    >
-                                        Xem báo cáo lỗi
-                                    </Button>
-                                    <Button
-                                        variant="outlined"
-                                        onClick={() => {
-                                            navigate("/exam/edit/" + exam.id);
-                                        }}
-                                    >
-                                        Chỉnh sửa
-                                    </Button>
-                                </>
-                            ) : (
-                                <AppModal
-                                    trigger={
-                                        <Button variant="outlined">
-                                            Báo cáo lỗi
-                                        </Button>
+                                    return;
+                                }
+                                if (exam.isFavorited) {
+                                    const res = await request.delete(
+                                        `exams/${exam.id}/favorite?userId=${userId}`
+                                    );
+                                    if (res) {
+                                        setExam(
+                                            (preValue) =>
+                                                ({
+                                                    ...preValue,
+                                                    isFavorited: false,
+                                                } as IDetailExam)
+                                        );
                                     }
+                                } else {
+                                    const res = await request.post(
+                                        `exams/${exam.id}/favorite?userId=${userId}`,
+                                        {}
+                                    );
+                                    if (res) {
+                                        setExam(
+                                            (preValue) =>
+                                                ({
+                                                    ...preValue,
+                                                    isFavorited: true,
+                                                } as IDetailExam)
+                                        );
+                                    }
+                                }
+                            }}
+                        />
+
+                        <Button
+                            variant="contained"
+                            onClick={() => {
+                                navigate("../practice/" + exam.id);
+                            }}
+                        >
+                            Thi thử
+                        </Button>
+                        <Button
+                            variant="outlined"
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                navigator.clipboard.writeText(
+                                    `${BASE_URL}exam/detail/${exam.id}?${
+                                        exam.isPublic
+                                            ? ""
+                                            : `securityCode=${exam.securityCode}`
+                                    }`
+                                );
+                                dispatch(
+                                    sendAlert({
+                                        message: "Đã copy đường đẫn",
+                                        time: 3,
+                                    })
+                                );
+                            }}
+                        >
+                            Chia sẻ
+                        </Button>
+                        {isOwner ? (
+                            <>
+                                <Button
+                                    variant="outlined"
+                                    onClick={() => {
+                                        navigate("/user/report");
+                                    }}
                                 >
-                                    <RepportError examId={exam.id} />
-                                </AppModal>
-                            )}
-                        </Stack>
-                    </Box>
-                    <ExamCarousel
-                        title="Các đề thi liên quan"
-                        exams={exam.relatedExams}
-                    />
-                </Stack>
-            );
-        } else {
-            return <Typography variant="h6">Đang tải dữ liệu</Typography>;
-        }
+                                    Xem báo cáo lỗi
+                                </Button>
+                                <Button
+                                    variant="outlined"
+                                    onClick={() => {
+                                        navigate("/exam/edit/" + exam.id);
+                                    }}
+                                >
+                                    Chỉnh sửa
+                                </Button>
+                            </>
+                        ) : (
+                            <AppModal
+                                trigger={
+                                    <Button variant="outlined">
+                                        Báo cáo lỗi
+                                    </Button>
+                                }
+                            >
+                                <RepportError examId={exam.id} />
+                            </AppModal>
+                        )}
+                    </Stack>
+                </Box>
+                <ExamCarousel
+                    title="Các đề thi liên quan"
+                    exams={exam.relatedExams}
+                />
+            </Stack>
+        );
+    } else {
+        return <Typography variant="h6">Đang tải dữ liệu</Typography>;
+    }
 }
