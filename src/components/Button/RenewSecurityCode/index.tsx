8821286c02a2507357f0caf44308222a07c732b5
luantran2k <tranluan2kyl@gmail.com>
2022-12-15 11:44:43 +0700

    fix security Code display first load && change document url after renew

--- src/components/Button/RenewSecurityCode/index.tsx
+++ src/components/Button/RenewSecurityCode/index.tsx
@@ -1,8 +1,9 @@
 import { Autorenew } from "@mui/icons-material";
 import { Stack, Typography, Button } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useAppDispatch } from "../../../app/hooks";
 import { setLoading } from "../../../redux/slices/appSlice";
+import { updateExam } from "../../../redux/slices/examSlice";
 import request from "../../../Utils/request";
 import styles from "./style.module.scss";
 
@@ -18,6 +19,9 @@ export default function RenewSecurityCodeButton(
     const [securityCode, setSecurityCode] = useState(securityCodeProp);
     const [isRenewing, setIsRenewing] = useState(false);
     const dispath = useAppDispatch();
+    useEffect(() => {
+        setSecurityCode(securityCodeProp);
+    }, [securityCodeProp]);
     return (
         <Stack direction="row" spacing={2} alignItems="center">
             <Typography>Mã xác minh: {securityCode}</Typography>
@@ -29,8 +33,9 @@ export default function RenewSecurityCodeButton(
                         any,
                         { securityCode: string; documentUrl: string }
                     >(`exams/${id}/changeSecurityCode`);
-                    if (res?.securityCode) {
+                    if (res?.documentUrl) {
                         setSecurityCode(res.securityCode);
+                        dispath(updateExam({ documentUrl: res.documentUrl }));
                     }
                     setIsRenewing(false);
                 }}
